8509d018643950ff7440080df34ba58a55ed884d
exxxar <exxxar@gmail.com>
2025-11-18 02:25:47 +0300

    update

--- resources/views/bot.blade.php
+++ resources/views/bot.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-bs-theme="light">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=0.8, maximum-scale=0.8, user-scalable=no">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
