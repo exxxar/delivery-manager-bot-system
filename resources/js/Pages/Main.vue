6864cb755c235e8a9ecdab7005c81cf3b0d8ffec
exxxar <exxxar@gmail.com>
2026-06-11 19:47:01 +0300

    add pwa format

--- resources/js/Pages/Main.vue
+++ resources/js/Pages/Main.vue
@@ -20,6 +20,28 @@ defineProps({
         </p>
     </div>
 
+    <div class="modal fade" id="installPwaModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    <h5 class="modal-title">Установить приложение</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+
+                <div class="modal-body">
+                    <p>Вы можете установить приложение к себе в один клик и запускать его прямо с рабочего стола.</p>
+                </div>
+
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" data-bs-dismiss="modal">Позже</button>
+                    <button class="btn btn-primary" @click="installPWA">Установить</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script>
@@ -61,8 +83,46 @@ export default {
 
     mounted() {
        // this.tg.requestFullscreen()
+
+        this.initPush()
     },
     methods: {
+        installPWA() {
+            window.installPWA()
+        },
+        async initPush() {
+
+            /*  const oldRegistration = await navigator.serviceWorker.ready
+              const oldSubscription =  await oldRegistration.pushManager.getSubscription()
+              if (oldSubscription) {
+                  oldSubscription.unsubscribe()
+                  console.log('Старая подписка удалена')
+              }
+  */
+            if (!('serviceWorker' in navigator) || !('PushManager' in window)) {
+                console.warn('Push notifications not supported')
+                return
+            }
+
+            const registration = await navigator.serviceWorker.register('/sw.js')
+
+            const permission = await Notification.requestPermission()
+            if (permission !== 'granted') {
+                console.warn('User denied notifications')
+                return
+            }
+
+            /*      const subscription = await registration.pushManager.subscribe({
+                      userVisibleOnly: true,
+                      applicationServerKey: this.vapidPublicKey,
+
+                  })
+      */
+            /*  await axios.post('/api/push/subscribe', {
+                  subscription,
+                  board_uuid: this.board.uuid
+              })*/
+        },
         open(url) {
             this.tg.openLink(url)
         },
